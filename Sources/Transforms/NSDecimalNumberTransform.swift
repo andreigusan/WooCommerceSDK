fb7f02d2a5181ee81a1990d147c615c900c83470
Thi <t@thi.im>
2016-04-10 12:08:52 +0900

    Add NSDecimalNumberTransform

---
+++ Sources/Transforms/NSDecimalNumberTransform.swift
@@ -0,0 +1,24 @@
+import Foundation
+import ObjectMapper
+
+public class NSDecimalNumberTransform: TransformType {
+    public typealias Object = NSDecimalNumber
+    public typealias JSON = String
+
+    public init() {}
+
+    public func transformFromJSON(value: AnyObject?) -> NSDecimalNumber? {
+        if let string = value as? String {
+            return NSDecimalNumber(string: string)
+        }
+        if let double = value as? Double {
+            return NSDecimalNumber(double: double)
+        }
+        return nil
+    }
+
+    public func transformToJSON(value: NSDecimalNumber?) -> String? {
+        guard let value = value else { return nil }
+        return value.description
+    }
+}
